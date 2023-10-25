6a84eddaa9e957c9e346ee800db4d778e309564f
Hunter Ambs <hunterambs@gmail.com>
2023-10-25 16:37:23 -0400

    fix

--- view_page.php
+++ view_page.php
@@ -34,7 +34,6 @@ if (isset($_GET['id'])) {
     echo "Invalid page ID.";
 }
 ?>
-<?php include('header.php'); ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -49,6 +48,9 @@ if (isset($_GET['id'])) {
     </style>
 </head>
 <body>
+    <main>
+    <?php include('header.php'); ?>
+    </main>
     <br><main>
     <h1><?php echo $pageTitle; ?></h1>
     </main><br>
