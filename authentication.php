78db479922c57a8702c713daa83a65b5131a895b
Hunter Ambs <hunterambs@gmail.com>
2023-10-25 18:26:31 -0400

    fix

--- authentication.php
+++ authentication.php
@@ -4,8 +4,8 @@ session_start();
 
 // Check if the user is not logged in
 if (!isset($_SESSION["user_id"])) {
-    // Redirect to the login page or another page with a message
-    header("Location: login.php");
+    // Redirect to the login page
+    header("Location: /");
     exit();
 }
 ?>
