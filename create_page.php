724f01604be379953443b4f0e86fb8e90c802a92
Hunter Ambs <hunterambs@gmail.com>
2023-10-25 00:13:43 -0400

    debugging

--- create_page.php
+++ create_page.php
@@ -27,34 +27,42 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         // Handle image upload
         $uploadDir = "/var/www/uploads/";
         $imagePath = $uploadDir . basename($_FILES["image"]["name"]);
-        
-        if (move_uploaded_file($_FILES["image"]["tmp_name"], $imagePath)) {
-            // Insert data into the database
-            $sql = "INSERT INTO user_pages (title, content, image_path) VALUES (?, ?, ?)";
-            $stmt = $conn->prepare($sql);
-            $stmt->bind_param("sss", $title, $content, $imagePath);
 
-            if ($stmt->execute()) {
-                // Get the ID of the newly created page
-                $newPageID = $stmt->insert_id;
+        if (isset($_FILES["image"])) {
+            if ($_FILES["image"]["error"] === UPLOAD_ERR_OK) {
+                if (move_uploaded_file($_FILES["image"]["tmp_name"], $imagePath)) {
+                    // Insert data into the database
+                    $sql = "INSERT INTO user_pages (title, content, image_path) VALUES (?, ?, ?)";
+                    $stmt = $conn->prepare($sql);
+                    $stmt->bind_param("sss", $title, $content, $imagePath);
 
-                // Close the prepared statement
-                $stmt->close();
+                    if ($stmt->execute()) {
+                        // Get the ID of the newly created page
+                        $newPageID = $stmt->insert_id;
 
-                // Close the database connection
-                $conn->close();
+                        // Close the prepared statement
+                        $stmt->close();
 
-                // Redirect the user to their new page
-                header("Location: view_page.php?id=$newPageID");
-                exit();
+                        // Close the database connection
+                        $conn->close();
+
+                        // Redirect the user to their new page
+                        header("Location: view_page.php?id=$newPageID");
+                        exit();
+                    } else {
+                        echo "Error: " . $stmt->error;
+                    }
+
+                    // Close the prepared statement
+                    $stmt->close();
+                } else {
+                    echo "Error moving uploaded image to the destination.";
+                }
             } else {
-                echo "Error: " . $stmt->error;
+                echo "File upload error: " . $_FILES["image"]["error"];
             }
-
-            // Close the prepared statement
-            $stmt->close();
         } else {
-            echo "Error uploading image.";
+            echo "Image not uploaded.";
         }
     }
 
