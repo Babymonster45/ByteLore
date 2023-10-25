1ee1cac4952b8134586b9c1c9cbab2b1f7f81d15
Hunter Ambs <hunterambs@gmail.com>
2023-10-25 16:04:20 -0400

    test

--- index.php
+++ index.php
@@ -18,24 +18,6 @@ session_start();
     <header>
         <h1>Welcome to ByteLore</h1>
     </header>
-    <nav>
-        <ul>
-            <li><a href="index.php">Home</a></li>
-            <li><a href="games_list.php">Game List</a></li>
-
-            <?php
-            // Check if the user is logged in and display appropriate buttons
-            if (isset($_SESSION["user_id"])) {
-                echo '<li><a href="logout.php">Logout</a></li>';
-            } else {
-                echo '<li><a href="login.php">Login</a></li>';
-                echo '<li><a href="signup.php">Sign Up</a></li>';
-            }
-            ?>
-
-        </ul>
-    </nav>
-
     <main>
         <br>
         <h2>View Games List</h2>
