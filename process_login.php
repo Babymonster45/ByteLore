e26ef186c19662ae999c6fcb04e423886091131d
Hunter Ambs <hunterambs@gmail.com>
2023-10-27 22:40:33 -0400

    remember me implementation

--- process_login.php
+++ process_login.php
@@ -6,6 +6,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Get user input from the login form
     $username = $_POST["username"];
     $password = $_POST["password"];
+    $remember_me = isset($_POST["remember_me"]);
 
     // Establish a database connection
     include('/secure_config/config.php');
@@ -27,6 +28,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         if (password_verify($password, $db_password_hash)) {
             // Password is correct
             $_SESSION["user_id"] = $user_id; // Set a session variable to indicate the user is logged in
+
+            // If "Remember Me" is checked, create a cookie to remember the user's login
+            if ($remember_me) {
+                $cookie_name = "remember_me_cookie";
+                $cookie_value = base64_encode($username . "|" . $db_password_hash);
+                setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/"); // 30 days
+            }
+
             header("Location: /"); // Redirect to the homepage or another page
         } else {
             // Password is incorrect
@@ -41,3 +50,4 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $stmt->close();
     $conn->close();
 }
+?>
