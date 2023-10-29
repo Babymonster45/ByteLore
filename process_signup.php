c01b672a47b1ededc8443df325b61a90d09ce92a
Hunter Ambs <hunterambs@gmail.com>
2023-10-29 18:49:51 -0400

    test

--- process_signup.php
+++ process_signup.php
@@ -8,6 +8,18 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = $_POST["email"];
     $password = $_POST["password"];
 
+    // Validate password
+    if (
+        strlen($password) < 8 ||                 // Minimum length of 8 characters
+        !preg_match('/[0-9]/', $password) ||     // At least 1 number
+        !preg_match('/[A-Z]/', $password) ||     // At least 1 uppercase character
+        !preg_match('/[a-z]/', $password) ||     // At least 1 lowercase character
+        !preg_match('/[\x21\x23\x24\x26\x28-\x2B\x2D\x3D\x3F\x40\x5B\x7E]/', $password) // At least 1 special character
+    ) {
+        header("Location: signup.php?error=2"); // Redirect back to the signup page with a password error message
+        exit();
+    }
+
     // Establish a database connection
     include('/secure_config/config.php');
 
@@ -15,55 +27,39 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         die("Connection failed: " . $conn->connect_error);
     }
 
-    // Verify that the password is at least 8 characters
-    if (mb_strlen($password, 'UTF-8') > 7) {
-        // Verify that the password has at least 1 lowercase, 1 uppercase, 1 number, and 1 special character
-        if (preg_match('/[a-z]/', $password) && preg_match('/[A-Z]/', $password) && preg_match('/[0-9]/', $password) && preg_match('/[\x21\x23\x24\x26\x28-\x2B\x2D\x3D\x3F\x40\x5B\x7E]/', $password)) {
-            // Hash the password before storing it in the database
-            $password_hash = password_hash($password, PASSWORD_DEFAULT);
+    // Hash the password before storing it in the database
+    $password_hash = password_hash($password, PASSWORD_DEFAULT);
 
-            // Check if the username or email is already in use
-            $checkQuery = "SELECT id FROM users WHERE username = ? OR email = ?";
-            $checkStmt = $conn->prepare($checkQuery);
-            $checkStmt->bind_param("ss", $username, $email);
-            $checkStmt->execute();
-            $checkStmt->store_result();
+    // Check if the username or email is already in use
+    $checkQuery = "SELECT id FROM users WHERE username = ? OR email = ?";
+    $checkStmt = $conn->prepare($checkQuery);
+    $checkStmt->bind_param("ss", $username, $email);
+    $checkStmt->execute();
+    $checkStmt->store_result();
 
-            if ($checkStmt->num_rows > 0) {
-                // Username or email is already in use
-                header("Location: signup.php?error=1"); // Redirect back to the signup page with an error message
-                exit();
-            }
-
-            // Insert the user into the database
-            $insertQuery = "INSERT INTO users (username, email, password_hash) VALUES (?, ?, ?)";
-            $insertStmt = $conn->prepare($insertQuery);
-            $insertStmt->bind_param("sss", $username, $email, $password_hash);
-
-            if ($insertStmt->execute()) {
-                // Registration was successful
-                $_SESSION["user_id"] = $insertStmt->insert_id; // Set a session variable to indicate the user is logged in
-                header("Location: /"); // Redirect to the homepage or another page
-            } else {
-                // Registration failed
-                $error_message = "Registration failed: " . $insertStmt->error;
-                header("Location: signup.php?error=" . urlencode($error_message));
-            }
-
-            // Close the prepared statements
-            $insertStmt->close();
-            $checkStmt->close();
-        } else {
-            // Password does not meet the requirements
-            header("Location: signup.php?error=2");
-            exit();
-        }
-    } else {
-        // Password is too short
-        header("Location: signup.php?error=3");
+    if ($checkStmt->num_rows > 0) {
+        // Username or email is already in use
+        header("Location: signup.php?error=1"); // Redirect back to the signup page with an error message
         exit();
     }
 
-    // Close the database connection
+    // Insert the user into the database
+    $insertQuery = "INSERT INTO users (username, email, password_hash) VALUES (?, ?, ?)";
+    $insertStmt = $conn->prepare($insertQuery);
+    $insertStmt->bind_param("sss", $username, $email, $password_hash);
+
+    if ($insertStmt->execute()) {
+        // Registration was successful
+        $_SESSION["user_id"] = $insertStmt->insert_id; // Set a session variable to indicate the user is logged in
+        header("Location: /"); // Redirect to the homepage or another page
+    } else {
+        // Registration failed
+        $error_message = "Registration failed: " . $insertStmt->error;
+        header("Location: signup.php?error=" . urlencode($error_message));
+    }
+
+    // Close database connections
+    $insertStmt->close();
+    $checkStmt->close();
     $conn->close();
 }
