e7a894a853d71489cd4aeac63b7b100d295e39ff
Hunter Ambs <hunterambs@gmail.com>
2023-11-21 02:27:44 -0500

    test

--- verify.php
+++ verify.php
@@ -8,38 +8,34 @@ if (isset($_GET['email']) && isset($_GET['token'])) {
     $email = $_GET['email'];
     $token = $_GET['token'];
 
-    // Check if the provided email and token match a record in the database
-    $checkTokenQuery = "SELECT * FROM users WHERE email = ? AND verification_token = ? AND is_verified = 0";
-    $checkTokenStmt = $conn->prepare($checkTokenQuery);
-    $checkTokenStmt->bind_param("ss", $email, $token);
-    $checkTokenStmt->execute();
-    $result = $checkTokenStmt->get_result();
+    // Check if the provided email exists in the database
+    $checkEmailQuery = "SELECT * FROM users WHERE email = ?";
+    $checkEmailStmt = $conn->prepare($checkEmailQuery);
+    $checkEmailStmt->bind_param("s", $email);
+    $checkEmailStmt->execute();
+    $result = $checkEmailStmt->get_result();
 
     if ($result->num_rows === 1) {
         // Fetch the user details
         $userDetails = $result->fetch_assoc();
+        $verification_token = $userDetails['verification_token'];
+        $is_verified = $userDetails['is_verified'];
 
-        // Extract username and password hash
-        $username = $userDetails['username'];
-        $password_hash = $userDetails['password_hash'];
+        if ($is_verified == 0 && $verification_token === $token) {
+            // Valid verification link; update the user's verification status
+            $updateQuery = "UPDATE users SET is_verified = 1 WHERE email = ?";
+            $updateStmt = $conn->prepare($updateQuery);
+            $updateStmt->bind_param("s", $email);
+            $updateStmt->execute();
 
-        // Update the user's verification status to mark it as verified
-        $updateQuery = "UPDATE users SET is_verified = 1 WHERE email = ?";
-        $updateStmt = $conn->prepare($updateQuery);
-        $updateStmt->bind_param("s", $email);
-        $updateStmt->execute();
-
-        $_SESSION["user_id"] = $userDetails["id"]; // Set a session variable to indicate the user is logged in
-        header("Location: /");
-        exit();
-    } else {
-        // Invalid verification link
-        header("Location: verification_failure.php");
-        exit();
+            $_SESSION["user_id"] = $userDetails["id"]; // Set a session variable to indicate the user is logged in
+            header("Location: /");
+            exit();
+        }
     }
-} else {
-    // Handle invalid verification requests
-    header("Location: verification_failure.php");
-    exit();
 }
+
+// Invalid verification link
+header("Location: verification_failure.php");
+exit();
 ?>
