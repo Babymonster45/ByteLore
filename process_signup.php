72d8e989e413ae66ff394ee0b703bbf4b286442f
Hunter Ambs <hunterambs@gmail.com>
2023-11-24 01:48:58 -0500

    test

--- process_signup.php
+++ process_signup.php
@@ -54,8 +54,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 
     // Validate email
-    $sender    = 'byteloreemail@gmail.com';
-    $validator = new SmtpEmailValidator(); //$email, $sender
+    $sender    = 'no-reply@platron.ru';
+    $validator = new SmtpEmailValidator($email, $sender);
 
     if (filter_var($email, FILTER_VALIDATE_EMAIL) === false || $validator->validate()) {
         $emailErrors[] = "Not a valid email address.";
