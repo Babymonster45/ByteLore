4458586536f92ccac12005f61d2b9fab89eebdd4
Hunter Ambs <hunterambs@gmail.com>
2023-11-24 04:10:50 -0500

    remember me cookie test

--- process_login.php
+++ process_login.php
@@ -46,15 +46,18 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             }
 
             header("Location: /"); // Redirect to the homepage or another page
+            exit();
         } else {
             // Password is incorrect
             $errorMessages[0] = "Username and/or password is incorrect.";
             header("Location: login.php?error=1"); // Redirect back to the login page with an error message
+            exit();
         }
     } else {
         // User not found
         $errorMessages[0] = "Username and/or password is incorrect.";
         header("Location: login.php?error=2"); // Redirect back to the login page with an error message
+        exit();
     }
 
     // If there are errors, redirect back to signup.php with the error messages
@@ -67,5 +70,27 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Close database connection
     $stmt->close();
     $conn->close();
+}else {
+    // Check if remember me cookie exists and log in user
+    if (isset($_COOKIE["remember_me_cookie"])) {
+        list($stored_username, $stored_password_hash) = explode("|", base64_decode($_COOKIE["remember_me_cookie"]));
+        
+        include('/secure_config/config.php');
+        
+        $query = "SELECT id, username FROM users WHERE username = ? AND password_hash = ?";
+        $stmt = $conn->prepare($query);
+        $stmt->bind_param("ss", $stored_username, $stored_password_hash);
+        $stmt->execute();
+        $stmt->bind_result($user_id, $db_username);
+        
+        if ($stmt->fetch()) {
+            $_SESSION["user_id"] = $user_id;
+            header("Location: /");
+            exit();
+        }
+        
+        $stmt->close();
+        $conn->close();
+    }
 }
 ?>
