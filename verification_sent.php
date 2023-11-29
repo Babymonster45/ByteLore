3bcdac5b664f7f663503018e1d248e93521d27e8
Hunter Ambs <hunterambs@gmail.com>
2023-11-29 17:15:23 -0500

    test

--- verification_sent.php
+++ verification_sent.php
@@ -20,7 +20,7 @@ if (isset($_SESSION["user_id"])) {
     <header>
         <h1>Verification Email Sent</h1>
     </header>
-    <main><br>
+    <main>
     <header class="subheader">
         <?php include('header.php'); ?>
     </header><br>
