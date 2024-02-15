f2f426e8e9078e95ccf9492f0cbfd5b2ce4da25d
Hunter Ambs <hunterambs@gmail.com>
2024-02-14 19:17:27 -0500

    update1

--- process_forgot_password.php
+++ process_forgot_password.php
@@ -54,6 +54,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         // Send an email with the reset link
         $resetLink = "https://cheeseindustries.de/reset_password.php?token=$resetToken";
+        $verification_message = "Here is your password reset link! Please click the following link to verify your account: <a href='$resetLink'>$resetLink</a>";
 
         $mail = new PHPMailer(true);
         try {
