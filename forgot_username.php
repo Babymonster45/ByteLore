b4f07716ebeeac6b14ac23d616894be5a1acfb02
Hunter Ambs <hunterambs@gmail.com>
2024-03-06 17:09:36 -0500

    test

---
+++ forgot_username.php
@@ -0,0 +1,37 @@
+<?php
+// Start a session to manage user login state
+session_start();
+
+// Check if the user is already logged in
+if (isset($_SESSION["user_id"])) {
+    header("Location: /");
+    exit();
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Forgot Username</title>
+    <link rel="stylesheet" href="login.css">
+</head>
+
+<body>
+    <header>
+        <h1>Forgot Username</h1>
+    </header>
+
+    <div class="subheader">
+        <?php include('header.php'); ?>
+    </div><br>
+
+    <form action="process_forgot_username.php" method="post">
+        <label for="email">Enter your email:</label>
+        <input type="email" name="email" required>
+        <input type="submit" value="Reset Username">
+    </form>
+</body>
+
+</html>
