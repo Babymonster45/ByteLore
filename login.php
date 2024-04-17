d5b603be237cc397bfb204a08effd126a0fa2d71
Hunter Ambs <hunterambs@gmail.com>
2024-04-17 18:07:19 -0400

    ads

--- login.php
+++ login.php
@@ -1,6 +1,6 @@
 <?php
 // Checks if the user tagged remember me
-include('remember_me.php');
+include ('remember_me.php');
 
 // Start a session to manage user login state
 if (session_status() == PHP_SESSION_NONE) {
@@ -36,32 +36,59 @@ if (isset($_SESSION["user_id"])) {
     </script>
 </head>
 
-<body>
-    <header>
-        <h1>Login</h1>
-    </header>
-    <div class="subheader">
-        <?php include('header.php'); ?>
-    </div><br>
-    <form action="process_login.php" method="post">
-        <label for="username">Username:</label>
-        <input type="text" name="username" id="username" required><br><br>
-
-        <label for="password">Password:</label>
-        <input type="password" name="password" id="password" required><br>
-
-        <!-- Remember me checkbox -->
-        <label for="remember_me">Remember Me:</label>
-        <input type="checkbox" name="remember_me" id="remember_me"> (30 days)</input>
-
-        <!-- Display the error message in red -->
-        <div class="error-message"></div>
-
-        <input class="button" type="submit" value="Login">
-    </form>
-    <p>Forgot Username? <a href="forgot_username.php">Reset it here</a></p>
-    <p>Forgot Password? <a href="forgot_password.php">Reset it here</a></p>
-    <p>Don't have an account? <a href="signup.php">Sign up</a></p>
-</body>
-
+<!-- Start Account Sign In Area -->
+<div class="account-login section">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-xl-6 col-lg-8">
+                <form class="card login-form inner-content" action="process_login.php" method="post">
+                    <div class="card-body">
+                        <div class="title">
+                            <h3>Sign In Now</h3>
+                            <p>Sign in by entering the information below.</p>
+                        </div>
+                        <div class="input-head">
+                            <div class="form-group input-group">
+                                <label> <i class="lni lni-user"></i> </label>
+                                <input class="form-control" type="text" name="username" id="username"
+                                    placeholder="Enter your user name" required />
+                            </div>
+                            <div class="form-group input-group">
+                                <label> <i class="lni lni-lock-alt"></i> </label>
+                                <input class="form-control" type="password" name="password" id="password"
+                                    placeholder="Enter your password" required />
+                            </div>
+                        </div>
+                        <!-- Display the error message in red -->
+                        <div class="error-message"></div>
+                        <div class="d-flex flex-wrap justify-content-between bottom-content">
+                            <div class="form-check">
+                                <input type="checkbox" name="remember_me" id="remember_me"
+                                    class="form-check-input width-auto" />
+                                <label class="form-check-label">Remember me</label>
+                            </div>
+                            <a class="lost-user" href="forgot_username.php">
+                                Forgot username?
+                            </a>
+                            <a class="lost-pass" href="forgot_password.php">
+                                Forgot password?
+                            </a>
+                        </div>
+                        <div class="light-rounded-buttons">
+                            <button class="btn primary-btn" type="submit">Sign In</button>
+                            <a href="signup.php" class="btn primary-btn-outline">
+                                Create Account
+                            </a>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
 </html>
+<!-- End Account Sign In Area -->
+
+<?php
+include 'views/footer.php';
+?>
