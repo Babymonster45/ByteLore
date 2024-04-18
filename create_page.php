4044de0558c038ed787daae07a9823e94a009751
Hunter Ambs <hunterambs@gmail.com>
2024-04-18 01:30:04 -0400

    asd

--- create_page.php
+++ create_page.php
@@ -221,7 +221,7 @@ include 'views/header.php';
         <input type="file" name="image" id="image" accept="image/*" class="btn primary-btn" hidden>
 
         <label for="content">Content:</label>
-        <textarea id="content" name="content" rows="10" cols="50" placeholder="Enter text here.." required></textarea>
+        <textarea id="content" name="content" rows="10" cols="50" placeholder="Enter text here.." required></textarea><br>
         <input class="btn primary-btn" type="submit" value="Create Page">
     </form>
 </body>
