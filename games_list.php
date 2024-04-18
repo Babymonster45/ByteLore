512c77b2fe0e4fe119ff514502948fe2ef24b362
Hunter Ambs <hunterambs@gmail.com>
2024-04-17 21:16:27 -0400

    asd

--- games_list.php
+++ games_list.php
@@ -61,16 +61,13 @@ include 'views/header.php';
                 <div class="col-lg-8">
                     <div class="call-action-content text-center">
                         <h2 class="action-title">Games List</h2>
-                        <div class="call-action-form">
-                            <form method="get" action="games_list.php">
-                                <input type="text" id="search" name="search" placeholder="Search for a game">
-                                <div class="action-btn rounded-buttons">
-                                    <button class="btn primary-btn rounded-full" type="submit">
-                                        search
-                                    </button>
-                                </div>
-                            </form>
-                        </div>
+                        <div class="search-container">
+        <form method="get" action="games_list.php">
+            <label for="search">Search by title:</label>
+            <input type="text" id="search" name="search" placeholder="Search for a game">
+            <button type="submit">Search</button>
+        </form>
+    </div>
                     </div>
                 </div>
             </div>
