e3ac942c5a9c8d945d1810ebeaf9b02e480cb2b6
Hunter Ambs <hunterambs@gmail.com>
2024-04-25 18:45:21 -0400

    asd

--- games_list.php
+++ games_list.php
@@ -86,12 +86,11 @@ include 'views/header.php';
             <div class="row justify-content-center">
                 <?php foreach ($pages as $page): ?>
                     <div class="col-lg-4 col-md-8 col-sm-10">
-                        <a href="view_page.php?id=<?php echo $page['id']; ?>" class="blog-link"
-                            style="text-decoration: none; color: inherit;">
+                        <a href="view_page.php?id=<?php echo $page['id']; ?>">
                             <div class="single-blog blog-style-one">
                                 <div class="blog-content">
                                     <h5 class="blog-title">
-                                        <?php echo $page['title']; ?>
+                                        <a><?php echo $page['title']; ?></a>
                                     </h5>
                                 </div>
                             </div>
