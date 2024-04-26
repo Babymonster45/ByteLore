d4ab0b952b33a6626bd784f0fedce48781bc37b3
Hunter Ambs <hunterambs@gmail.com>
2024-04-25 23:41:48 -0400

    asd

--- verification_sent.php
+++ verification_sent.php
@@ -1,6 +1,6 @@
 <?php
 // Checks if the user tagged remember me
-include('remember_me.php');
+include ('remember_me.php');
 
 // Start a session to manage user login state
 if (session_status() == PHP_SESSION_NONE) {
@@ -13,32 +13,36 @@ if (isset($_SESSION["user_id"])) {
     exit();
 }
 
+include 'views/pageBuilder.php';
+include 'views/header.php';
 ?>
-<!DOCTYPE html>
-<html lang="en">
 
 <head>
-    <meta charset="UTF-8">
     <title>Verification Email Sent</title>
-    <link rel="stylesheet" href="verification.css">
 </head>
 
-<body><br>
-    <header>
-        <h1>Verification Email Sent</h1>
-    </header>
-    <div>
-        <?php include('header.php'); ?>
-    </div><br>
-    <main>
-        <form>
-            <h4>
-                <p>A verification email has been sent to your provided email address.</p>
-                <p>Please check your inbox (and spam/junk folder) for the verification email.</p>
-                <p>Click the link in the email to verify your account.</p>
-            </h4>
-        </form>
-    </main>
-</body>
+<!--====== CALL TO ACTION FOUR PART START ======-->
+<section class="call-action-area call-action-four">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-lg-8">
+                <div class="call-action-content text-center">
+                    <h2 class="action-title">Verification Email Sent</h2>
+                    <p class="text">
+                        A verification email has been sent to your provided email address.<br />
+                        Please check your inbox (and spam/junk folder) for the verification email.<br />
+                        Click the link in the email to verify your account.
+                    </p>
+                </div>
+                <!-- call action content -->
+            </div>
+        </div>
+        <!-- row -->
+    </div>
+    <!-- container -->
+</section>
+<!--====== CALL TO ACTION FOUR PART ENDS ======-->
 
-</html>
+<?php
+include 'views/footer.php';
+?>
