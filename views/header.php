654a039a0dba08bb2fede05ddf613989bdf4eb16
Hunter Ambs <hunterambs@gmail.com>
2024-04-26 00:24:38 -0400

    asd

--- views/header.php
+++ views/header.php
@@ -3,7 +3,7 @@
    <div class="container">
       <div class="row">
          <div class="col-lg-12">
-            <nav class="navbar navbar-expand-lg" style="display: grid; grid-template-columns: 1fr 1fr 1fr;">
+            <nav class="navbar navbar-expand-lg">
                <a class="navbar-brand Headerlogo" href="./">
                <img style= "max-width:60%;" src="./web/img/logo-2.png" alt="Logo"/>
                </a>
@@ -20,7 +20,7 @@
                <span class="toggler-icon"></span>
                <span class="toggler-icon"></span>
                </button>
-               <div class="collapse navbar-collapse sub-menu-bar" id="navbarOne" style="justify-self: center;">
+               <div class="collapse navbar-collapse sub-menu-bar" id="navbarOne">
                   <ul class="navbar-nav m-auto">
                      <li class="nav-item">
                         <a href="/">Home</a>
@@ -28,17 +28,13 @@
                      <li class="nav-item">
                         <a href="/games_list.php">Game List</a>
                      </li>
-                  </ul>
-               </div>
-               <div class="navbar-btn d-none d-sm-inline-block" style="width: 200px; justify-self: end;">
-                  <ul>
                      <?php
                      // Check if the user is logged in and display appropriate buttons
                      if (isset($_SESSION["user_id"])) {
-                         echo '<li><a class="btn primary-btn-outline" href="logout.php">Logout</a></li>';
+                         echo '<li class="nav-item"><a href="logout.php">Logout</a></li>';
                      } else {
-                         echo '<li><a class="btn primary-btn-outline" href="login.php">Sign In</a></li>';
-                         echo '<li><a class="btn primary-btn" href="signup.php">Sign Up</a></li>';
+                         echo '<li class="nav-item"><a href="login.php">Sign In</a></li>';
+                         echo '<li class="nav-item"><a href="signup.php">Sign Up</a></li>';
                      }
                      ?>
                   </ul>
