a9638fbfd405043368f8655c6d21d3ac937ca0da
Hunter Ambs <hunterambs@gmail.com>
2024-04-26 00:48:53 -0400

    asd

--- views/header.php
+++ views/header.php
@@ -3,21 +3,24 @@
    <div class="container">
       <div class="row">
          <div class="col-lg-12">
-            <nav class="navbar navbar-expand-lg" style="display: grid; grid-template-columns: 1fr 1fr 1fr;">
+            <nav class="navbar navbar-expand-lg">
                <a class="navbar-brand Headerlogo" href="./">
                <img style= "max-width:60%;" src="./web/img/logo-2.png" alt="Logo"/>
                </a>
-               <div class="collapse navbar-collapse sub-menu-bar" id="navbarOne" style="justify-self: center;">
+               <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarOne" aria-controls="navbarOne" aria-expanded="false" aria-label="Toggle navigation">
+                  <span class="navbar-toggler-icon"></span>
+               </button>
+               <div class="collapse navbar-collapse sub-menu-bar" id="navbarOne">
                   <ul class="navbar-nav m-auto">
                      <li class="nav-item">
-                        <a href="/">Home</a>
+                        <a class="nav-link" href="/">Home</a>
                      </li>
                      <li class="nav-item">
-                        <a href="/games_list.php">Game List</a>
+                        <a class="nav-link" href="/games_list.php">Game List</a>
                      </li>
                   </ul>
                </div>
-               <div class="navbar-btn d-none d-sm-inline-block" style="width: 200px; justify-self: end;">
+               <div class="navbar-btn d-none d-sm-inline-block">
                   <ul>
                      <?php
                      // Check if the user is logged in and display appropriate buttons
