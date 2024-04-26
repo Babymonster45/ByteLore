8ce588004036cb01c233bbd87ac2707900e544f9
Hunter Ambs <hunterambs@gmail.com>
2024-04-26 00:18:11 -0400

    asd

--- views/header.php
+++ views/header.php
@@ -1,33 +1,63 @@
-<!--====== NAVBAR ONE PART START ======-->
-<section class="navbar-area navbar-one">
+ <!--====== NAVBAR ONE PART START ======-->
+ <section class="navbar-area navbar-one">
    <div class="container">
       <div class="row">
          <div class="col-lg-12">
-            <nav class="navbar navbar-expand-md" style="display: grid; grid-template-columns: 1fr 1fr 1fr;">
+            <nav class="navbar navbar-expand-lg">
                <a class="navbar-brand Headerlogo" href="./">
                <img style= "max-width:60%;" src="./web/img/logo-2.png" alt="Logo"/>
                </a>
-               <div class="collapse navbar-collapse sub-menu-bar" id="navbarOne" style="justify-self: center;">
+               <button
+                  class="navbar-toggler"
+                  type="button"
+                  data-bs-toggle="collapse"
+                  data-bs-target="#navbarOne"
+                  aria-controls="navbarOne"
+                  aria-expanded="false"
+                  aria-label="Toggle navigation"
+                  >
+               <span class="toggler-icon"></span>
+               <span class="toggler-icon"></span>
+               <span class="toggler-icon"></span>
+               </button>
+               <div class="collapse navbar-collapse sub-menu-bar" id="navbarOne">
                   <ul class="navbar-nav m-auto">
                      <li class="nav-item">
-                        <a href="/">Home</a>
+                        <a
+                           class="page-scroll active"
+                           href="./"
+                           data-bs-toggle="collapse"
+                           data-bs-target="#sub-nav1"
+                           aria-controls="sub-nav1"
+                           aria-expanded="false"
+                           aria-label="Toggle navigation"
+                           >
+                           Home
+                        </a>
                      </li>
                      <li class="nav-item">
                         <a href="/games_list.php">Game List</a>
                      </li>
+                     <li class="nav-item">
+                        <a href="javascript:void(0)">Resources</a>
+                     </li>
+                     <li class="nav-item">
+                        <a href="javascript:void(0)">Support</a>
+                     </li>
                   </ul>
                </div>
-               <div class="navbar-btn d-none d-sm-inline-block" style="width: 200px; justify-self: end;">
+               <div class="navbar-btn d-none d-sm-inline-block">
                   <ul>
-                     <?php
-                     // Check if the user is logged in and display appropriate buttons
-                     if (isset($_SESSION["user_id"])) {
-                         echo '<li><a class="btn primary-btn-outline" href="logout.php">Logout</a></li>';
-                     } else {
-                         echo '<li><a class="btn primary-btn-outline" href="login.php">Sign In</a></li>';
-                         echo '<li><a class="btn primary-btn" href="signup.php">Sign Up</a></li>';
-                     }
-                     ?>
+                     <li>
+                        <a class="btn primary-btn-outline" href="./login.php"
+                           >Sign In</a
+                           >
+                     </li>
+                     <li>
+                        <a class="btn primary-btn" href="./signup.php"
+                           >Sign Up</a
+                           >
+                     </li>
                   </ul>
                </div>
             </nav>
