8fee4a6c47640c7852dbf3b6c093b7d8895cb706
Hunter Ambs <hunterambs@gmail.com>
2024-04-29 22:05:49 -0400

    asd

--- views/header.php
+++ views/header.php
@@ -7,22 +7,25 @@
                <a class="navbar-brand Headerlogo" href="./">
                   <img style="max-width:60%;" src="./web/img/logo-2.png" alt="Logo" />
                </a>
-               <button class="navbar-toggler">
+               <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarOne"
+                  aria-controls="navbarOne" aria-expanded="false" aria-label="Toggle navigation">
                   <span class="toggler-icon"></span>
                   <span class="toggler-icon"></span>
                   <span class="toggler-icon"></span>
                </button>
-               <div class="collapse navbar-collapse sub-menu-bar" id="navbarOne" style="justify-self: center;">
+               <div class="collapse navbar-collapse sub-menu-bar" id="navbarOne">
                   <ul class="navbar-nav m-auto">
                      <li class="nav-item">
-                        <a href="/">Home</a>
+                        <a class="page-scroll" href="/">
+                           Home
+                        </a>
                      </li>
                      <li class="nav-item">
                         <a href="/games_list.php">Game List</a>
                      </li>
                   </ul>
                </div>
-               <div class="navbar-btn d-none d-sm-inline-block" style="width: 200px; justify-self: end;">
+               <div class="navbar-btn d-none d-sm-inline-block">
                   <ul>
                      <?php
                      // Check if the user is logged in and display appropriate buttons
